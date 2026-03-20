e2aaa95bbc6de626fb1c69257de5fd8e17390adb
Josh Girvin <josh@jgirvin.com>
2026-03-20 22:42:34 +1000

    Adding basic arg reading

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,16 @@
-let () = Printf.printf "%s\n" "Hello, world!"
+let has_posts path =
+  path
+  |> Sys.readdir
+  |> Array.exists (String.equal "posts")
+
+let usage_message = "jgirvin_blog <path>"
+let path = ref ""
+let store_path arg =
+  path := arg
+
+let run path =
+  Printf.printf "%b" (has_posts path)
+
+let () =
+  Arg.parse [] store_path usage_message;
+  Printf.printf "%b\n" (has_posts !path)
