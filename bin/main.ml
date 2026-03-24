c4e15551b0f7337c4742f56abfa0f03de5b8d333
Josh Girvin <josh@jgirvin.com>
2026-03-24 22:26:03 +1000

    Cleaning up error handling

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,5 @@
+open Jgirvin_blog
+
 let input_dir = ref "."
 let output_dir = ref "build"
 
@@ -7,13 +9,21 @@ let spec =
     ("--output", Arg.Set_string output_dir, "<dir> Output path (default: build)");
   ]
 
+let fail msg =
+  Printf.eprintf "Error: %s\n" msg;
+  exit 1
+
 let () =
   let usage = "jgirvin_blog [options]" in
   Arg.parse spec
     (fun _ -> raise (Arg.Bad "unexpected anonymous argument"))
     usage;
   if String.equal !input_dir !output_dir then
-    raise (Arg.Bad "input and output dirs can't be equal");
+    fail "input and output dirs can't be equal";
+  if not (is_valid_input_dir !input_dir) then
+    fail "input directory must contain posts/ pages/ templates/ subdirectories";
+  if not (is_valid_output_dir !output_dir) then
+    fail "output directory must exist and be a directory";
   Printf.printf "input=%s output=%s\n" !input_dir !output_dir;
   Printf.printf "has posts in input: %b\n"
     (Jgirvin_blog.contains_dir Posts !input_dir)
