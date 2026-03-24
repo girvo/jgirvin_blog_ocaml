0ec2f416f588eba00415132cb1962a2147a46f76
Josh Girvin <josh@jgirvin.com>
2026-03-24 22:29:40 +1000

    Can now tell whats in the directories, nice

--- bin/main.ml
+++ bin/main.ml
@@ -19,11 +19,11 @@ let () =
     (fun _ -> raise (Arg.Bad "unexpected anonymous argument"))
     usage;
   if String.equal !input_dir !output_dir then
-    fail "input and output dirs can't be equal";
+    fail "input and output dirs can't be the same";
   if not (is_valid_input_dir !input_dir) then
-    fail "input directory must contain posts/ pages/ templates/ subdirectories";
+    fail
+      ("input directory must contain posts/ pages/ templates/ subdirectories: "
+     ^ !input_dir);
   if not (is_valid_output_dir !output_dir) then
-    fail "output directory must exist and be a directory";
-  Printf.printf "input=%s output=%s\n" !input_dir !output_dir;
-  Printf.printf "has posts in input: %b\n"
-    (Jgirvin_blog.contains_dir Posts !input_dir)
+    fail ("output directory must exist and be a directory: " ^ !output_dir);
+  Printf.printf "input=%s output=%s\n" !input_dir !output_dir
